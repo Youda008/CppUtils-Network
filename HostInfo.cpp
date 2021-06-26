f3b921b77a9f287494e1a2b731854bd8d5c1024d
Youda <youda008@gmail.com>
2021-06-26 15:18:53 +0200

    host name to IP resolution

---
+++ HostInfo.cpp
@@ -0,0 +1,56 @@
+//======================================================================================================================
+// Project: CppUtils
+//----------------------------------------------------------------------------------------------------------------------
+// Author:      Jan Broz (Youda008)
+// Description: classes for network addresses
+//======================================================================================================================
+
+#include "HostInfo.hpp"
+
+#include "Essential.hpp"
+
+#include "CriticalError.hpp"
+
+#ifdef _WIN32
+	#include <ws2tcpip.h>
+#else
+	#include <arpa/inet.h>
+#endif
+
+
+namespace own {
+
+
+//======================================================================================================================
+
+AddrResult getAddrByHostname( const std::string & hostName )
+{
+	AddrResult result;
+
+	struct addrinfo hint;
+	memset( &hint, 0, sizeof(hint) );
+	hint.ai_family = AF_UNSPEC;      // IPv4 or IPv6, it doesn't matter
+
+	struct addrinfo * ainfo;
+	if (::getaddrinfo( hostName.c_str(), nullptr, &hint, &ainfo ) != 0)
+	{
+		result.error = getLastError();
+	}
+	else
+	{
+		result.error = 0;
+		Endpoint endpoint;
+		sockaddrToEndpoint( ainfo->ai_addr, endpoint );
+		result.addr = endpoint.addr;
+	}
+
+	freeaddrinfo( ainfo );
+
+	return result;
+}
+
+
+//======================================================================================================================
+
+
+} // namespace own
